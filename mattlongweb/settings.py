74b89c7e84df61855368965c41b342027b801491
Matt Long <matt@crocodoc.com>
2012-08-29 01:03:45 -0700

    settings should switch properly for production vs development

--- mattlongweb/settings.py
+++ mattlongweb/settings.py
@@ -2,8 +2,10 @@ import os, json
 
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-DEBUG = PROJECT_DIR.find('www') == -1
-TEMPLATE_DEBUG = DEBUG
+IS_PRODUCTION = os.environ.get('MATTLONG_ORG_PRODUCTION') == 'true'
+
+TEMPLATE_DEBUG = \
+DEBUG = not IS_PRODUCTION
 
 MANAGERS = ADMINS = (('Matt Long', 'matt@mattlong.org'),)
 
@@ -18,24 +20,6 @@ DATABASES = {
     },
 }
 
-INTERNAL_IPS = ('127.0.0.1',)
-DEBUG_TOOLBAR_CONFIG = {
-    'HIDE_DJANGO_SQL': True,
-    'ENABLE_STACKTRACES': False,
-    'INTERCEPT_REDIRECTS': False,
-}
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    #'debug_toolbar.panels.logger.LoggingPanel',
-)
-
 TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
@@ -43,22 +27,18 @@ USE_I18N = False
 USE_L10N = False
 USE_TZ = True
 
+SECRET_KEY = '2d*1aum8)r9gcyd*m)vm=-)b#4!#2&amp;g!#@=o6#k+h2uqy$1*6o'
+
 MEDIA_ROOT = ''
 MEDIA_URL = ''
-if DEBUG:
-    STATIC_ROOT = ''
-    STATIC_URL = '/static/'
-else:
-    STATIC_ROOT = '/home/ubuntu/www/mattlong-static'
-    STATIC_URL = '//static.mattlong.org'
-STATICFILES_DIRS = ()
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = ()
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-
-SECRET_KEY = '2d*1aum8)r9gcyd*m)vm=-)b#4!#2&amp;g!#@=o6#k+h2uqy$1*6o'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -74,7 +54,6 @@ MIDDLEWARE_CLASSES = (
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'toolbox.middleware.LogUnhandledExceptions',
 )
 
@@ -101,7 +80,6 @@ INSTALLED_APPS = (
 
     #thirdparty
     'south',
-    'debug_toolbar',
 
     #mine
     'toolbox',
@@ -111,17 +89,14 @@ INSTALLED_APPS = (
     'bookmarks',
 )
 
+AUTH_PROFILE_MODULE = 'base.models.UserProfile'
+
 OAUTH_EMAIL_MAP = json.loads(os.environ.get('OAUTH_EMAIL_MAP', '{}'))
-OAUTH_DEBUG_CODE = os.environ.get('OAUTH_DEBUG_CODE')
-OAUTH_DEBUG_ACCESS_TOKEN = os.environ.get('OAUTH_DEBUG_ACCESS_TOKEN')
+OAUTH_DEBUG_CODE = None
+OAUTH_DEBUG_ACCESS_TOKEN = None
 GOOGLE_API_CLIENT_ID = os.environ.get('GOOGLE_API_CLIENT_ID')
 GOOGLE_API_CLIENT_SECRET = os.environ.get('GOOGLE_API_CLIENT_SECRET')
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -153,4 +128,30 @@ LOGGING = {
     }
 }
 
-AUTH_PROFILE_MODULE = 'YOURAPP.UserProfile'
+if IS_PRODUCTION:
+    SECRET_KEY = os.environ['MATTLONG_ORG_SECRET_KEY']
+    STATIC_ROOT = '/home/ubuntu/www/mattlong-static'
+    STATIC_URL = '//static.mattlong.org'
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+    DEBUG_TOOLBAR_CONFIG = {
+        'HIDE_DJANGO_SQL': True,
+        'ENABLE_STACKTRACES': False,
+        'INTERCEPT_REDIRECTS': False,
+    }
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        #'debug_toolbar.panels.logger.LoggingPanel',
+    )
+    OAUTH_DEBUG_CODE = os.environ.get('OAUTH_DEBUG_CODE')
+    OAUTH_DEBUG_ACCESS_TOKEN = os.environ.get('OAUTH_DEBUG_ACCESS_TOKEN')
