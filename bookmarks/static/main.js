e2270c83edf9b6af9050e5d42f76dc140fe1b775
Matt Long <matt@crocodoc.com>
2012-08-31 00:29:25 +0000

    making bookmarklet javascript injection work more universally
    it turns out that document.documentElement.childNodes[0] can quite
    easily not be a usefule node. e.g. if <!--<![endif]--> comes right after
    the <html> tag

--- bookmarks/static/main.js
+++ bookmarks/static/main.js
@@ -1,5 +1,5 @@
 // based on http://benalman.com/projects/run-jquery-code-bookmarklet/
-(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.7.2",function($,L){
+(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.body.appendChild(c)}})(window,document,"1.7.2",function($,L){
 
     var l = document.location;
     var title = document.title;
