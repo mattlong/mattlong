df042d0240a8bbe4a0cce724978632af300e7b42
Matt Long <matt@crocodoc.com>
2012-12-16 20:20:27 +0000

    adding newrelic support

--- mattlongweb/wsgi.py
+++ mattlongweb/wsgi.py
@@ -26,3 +26,8 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+if 'NEW_RELIC_CONFIG_FILE' in os.environ:
+    import newrelic.agent
+    newrelic.agent.initialize(os.environ['NEW_RELIC_CONFIG_FILE'])
+    application = newrelic.agent.wsgi_application()(application)
